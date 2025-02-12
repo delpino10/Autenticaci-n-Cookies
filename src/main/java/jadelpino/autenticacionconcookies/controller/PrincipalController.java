a829051319b37a7c3c1c00108f0142599e44e438
Jose Antonio del Pino <jadelpino10@gmail.com>
2025-02-12 21:40:41 +0100

    Cookies añadidas y sus respectivos contadores

--- src/main/java/jadelpino/autenticacionconcookies/controller/PrincipalController.java
+++ src/main/java/jadelpino/autenticacionconcookies/controller/PrincipalController.java
@@ -1,12 +1,12 @@
 package jadelpino.autenticacionconcookies.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 import java.util.HashMap;
@@ -51,11 +51,39 @@ public class PrincipalController {
         public String updatePassword(
             @RequestParam("contrasenia") String password,
             HttpSession session,
+            HttpServletResponse response,
+            HttpServletRequest request,
             Model model) {
 
             // Obtener el usuario de la sesión
             String usuario = (String) session.getAttribute("usuario");
+            int contador = 0;
             if (usuarios.containsKey(usuario) && password.equals(usuarios.get(usuario))) {
+                Cookie[] cookies = request.getCookies();
+                for (Cookie cookie : cookies) {
+                    if (cookie.getName().equals(usuario)) {
+                        try {
+                            contador = Integer.parseInt(cookie.getValue().split("=")[1]);
+                        }catch (NumberFormatException  | ArrayIndexOutOfBoundsException e) {
+                            contador = 0;
+                        }
+                    }
+                }
+
+
+                // Incrementar contador de visitas
+                contador++;
+
+                //Serializar de Nuevo
+                String nuevoValor = "contador=" + contador ;
+                // Crear una nueva cookie con el contador actualizado
+                Cookie nuevaCookie = new Cookie(usuario, nuevoValor);
+                nuevaCookie.setPath("/"); // Disponible en toda la aplicación
+                response.addCookie(nuevaCookie);
+
+
+                model.addAttribute("usuario", "Bienvenido " + usuario + "!");
+                model.addAttribute("contador", contador);
                 return "areaPersonal";
             }
             model.addAttribute("error", "El la contraseña "+ usuario +" no es correcta");
--- src/main/resources/static/js/funciones.js
+++ src/main/resources/static/js/funciones.js
@@ -1,6 +1,7 @@
+
+
 // ## Contraseñas #######
 // Revela el texto insertado en los inputs passwords
-
     function verContrasenia() {
         var passwordField = document.getElementById("contrasenia");
         var checkbox = document.getElementById("checkbox");
